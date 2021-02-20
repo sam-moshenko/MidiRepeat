0489f1248eddc446710dfd9a32f485bfeeb95db9
Simon <u11821@alfabank.kz>
2021-02-20 23:31:36 +0600

    Initial Commit

---
+++ Shared/MidiRepeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidiRepeatApp.swift
+//  Shared
+//
+//  Created by Simon Lebedev on 20.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct MidiRepeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
