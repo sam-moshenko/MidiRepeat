c55f0a20574b19d3bc7f42b18ea4431112330957
Simon <u11821@alfabank.kz>
2021-03-26 02:33:21 +0600

    Fix piano key jumping

--- Shared/Views/PianoView.swift
+++ Shared/Views/PianoView.swift
@@ -14,6 +14,19 @@ struct PianoView: View {
     var whiteNotes: [Note] {
         notes.filter { $0.isWhite }
     }
+    var blackNotesWithSpacings: [(spacing: CGFloat, note: Note)] {
+        var spacing: CGFloat = 0
+        return notes.map { note -> (spacing: CGFloat, note: Note)? in
+            if note.isWhite {
+                spacing += 1
+                return nil
+            } else {
+                let _spacing = spacing
+                spacing = -1 / 4
+                return (spacing: _spacing - 1 / 4, note: note)
+            }
+        }.compactMap { $0 }
+    }
     
     var body: some View {
         GeometryReader { geometry in
@@ -44,33 +57,23 @@ struct PianoView: View {
                 .frame(height: geometry.size.height)
                 .fixedSize()
                 HStack(spacing: 0) {
-                    var spacing: CGFloat = 0
-                    ForEach(notes, id: \.self) { (note) -> AnyView? in
-                        if note.isWhite {
-                            spacing += whiteWidth
-                            return nil
-                        } else {
-                            let _spacing = spacing
-                            spacing = -blackWidth / 2
-                            return AnyView(
-                                HStack(spacing: 0) {
-                                    Spacer(minLength: _spacing - blackWidth / 2)
-                                    Rectangle()
-                                        .foregroundColor(Color(white: 0.1))
-                                        .frame(width: blackWidth)
-                                        .contentShape(Rectangle())
-                                        .gesture(
-                                            DragGesture(
-                                                minimumDistance: 0,
-                                                coordinateSpace: .local
-                                            ).onChanged { _ in
-                                                playedNotes.insert(note)
-                                            }.onEnded { _ in
-                                                playedNotes.remove(note)
-                                            }
-                                        )
-                                }
-                            )
+                    ForEach(blackNotesWithSpacings, id: \.note) { (spacing, note) in
+                        HStack(spacing: 0) {
+                            Spacer(minLength: spacing * whiteWidth)
+                            Rectangle()
+                                .foregroundColor(Color(white: 0.1))
+                                .frame(width: blackWidth)
+                                .contentShape(Rectangle())
+                                .gesture(
+                                    DragGesture(
+                                        minimumDistance: 0,
+                                        coordinateSpace: .local
+                                    ).onChanged { _ in
+                                        playedNotes.insert(note)
+                                    }.onEnded { _ in
+                                        playedNotes.remove(note)
+                                    }
+                                )
                         }
                     }
                 }
